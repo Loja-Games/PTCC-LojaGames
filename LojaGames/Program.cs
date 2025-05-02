8e3d3df3ad986b36f4603767e84ebdba9faf5b1d
Jean Olwrick Badette <wils.jhon38@mail.com>
2025-05-02 20:26:40 -0300

    Adicionando o html na views de login, colocar o comando para conectar ao banco no appsettings.json, adicionando no layout o login

--- LojaGames/Program.cs
+++ LojaGames/Program.cs
@@ -1,7 +1,13 @@
+using LojaGames.Controllers;
+using LojaGames.Models;
+using LojaGames.Repositorios;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
