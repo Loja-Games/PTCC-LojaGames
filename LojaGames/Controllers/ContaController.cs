e185ca51526790ee9828a50216a921e97cfaa1ac
joaopaulo10101010 <joao_paulo_oandrade@hotmail.com>
2025-05-03 22:45:06 -0300

    fazendo o mecanismo funcional de login, e criando a controler da conta

---
+++ LojaGames/Controllers/ContaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaGames.Controllers
+{
+    public class ContaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Conta()
+        {
+            return View();
+        }
+    }
+}
--- LojaGames/Controllers/LoginControler.cs
+++ LojaGames/Controllers/LoginControler.cs
@@ -1,4 +1,5 @@
-﻿using LojaGames.Repositorios;
+﻿using LojaGames.Models;
+using LojaGames.Repositorios;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LojaGames.Controllers
@@ -18,8 +19,28 @@ namespace LojaGames.Controllers
             return View();
         }
 
-        public IActionResult Login() 
+        public IActionResult Login()
         {
+            // retorna a página Login
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string cpf, string senha)
+        {
+
+            Tb_usuario tb_Usuario = new Tb_usuario();
+            tb_Usuario.Cpf_cli = cpf;
+            tb_Usuario.Senha_cli = senha;
+
+            var usuario = _usuarioRepositorio.ObterUsuarioCpf(tb_Usuario);
+
+            if (usuario != null && tb_Usuario.Senha_cli == usuario.Senha_cli && tb_Usuario.Cpf_cli == usuario.Cpf_cli)
+            {
+                return RedirectToAction("Conta", "Conta"); /* Destino Após o login */
+            }
+
+            ModelState.AddModelError("", "Os dados de Login são invalidos.");
             return View();
         }
     }
