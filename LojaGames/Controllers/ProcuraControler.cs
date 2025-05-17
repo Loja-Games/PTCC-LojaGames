29af1c918d8eccd62a3a4f596c035a1279106a17
joaopauloo10101010 <joao_paulo_oandrade@hotmail.com>
2025-05-17 01:56:12 -0300

    criado a nova view da aba procura

---
+++ LojaGames/Controllers/ProcuraControler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaGames.Controllers
+{
+    public class ProcuraControler : Controller
+    {
+        public IActionResult Procura()
+        {
+            return View();
+        }
+    }
+}
