918fe28439c7239eb821cf35547a13cdbedb27cc
joaopauloo10101010 <joao_paulo_oandrade@hotmail.com>
2025-05-25 20:33:25 -0300

    começando o formulario de pagamento

---
+++ LojaGames/Controllers/PagamentoControler.cs
@@ -0,0 +1,25 @@
+﻿using LojaGames.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaGames.Controllers
+{
+    public class PagamentoControler : Controller
+    {
+        private readonly ProdutoRepositorio _produtoRepositorio;
+
+        public PagamentoControler(ProdutoRepositorio produtoRepositorio)
+        {
+         _produtoRepositorio = produtoRepositorio;
+        }
+
+
+
+        public IActionResult Pagamento()
+        {
+
+            _produtoRepositorio.listadeprodutoserdados.listadeprodutos = _produtoRepositorio.ListaProdutos();
+            _produtoRepositorio.listadeprodutoserdados.listacarrinho = _produtoRepositorio.listaCarrinho(HttpContext.Session.GetString("Pedido"));
+            return View(_produtoRepositorio.listadeprodutoserdados);
+        }
+    }
+}
