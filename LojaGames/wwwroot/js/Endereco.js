930caefc89ba2f4a5184129ba2351e9e04108ae6
joaopauloo10101010 <joao_paulo_oandrade@hotmail.com>
2025-06-01 17:56:22 -0300

    adicionando metodo de contato com a API para os enderesos

---
+++ LojaGames/wwwroot/js/Endereco.js
@@ -0,0 +1,43 @@
+﻿const inputcep = document.getElementById("inputcep");
+const inputendereco = document.getElementById("inputendereco");
+const inputnumero = document.getElementById("inputnumero");
+const inputcomplemento = document.getElementById("inputcomplemento");
+const inputbairro = document.getElementById("inputbairro");
+
+inputcep.addEventListener('input', async () => {
+    if (inputcep.value.length == 8) {
+        let resposta = await contatarAPICorreios(inputcep.value);
+        if (resposta.logradouro != null && resposta.bairro != null) {
+            inputendereco.value = resposta.logradouro;
+            inputbairro.value = resposta.bairro;
+        }
+    }
+});
+
+async function contatarAPICorreios(cep) {
+    try {
+        const API = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await API.json();
+        const retorno = {
+            bairro: data.bairro,
+            cep: data.cep,
+            complemento: data.complemento,
+            ddd: data.ddd,
+            estado: data.estado,
+            gia: data.gia,
+            ibge: data.ibge,
+            localidade: data.localidade,
+            logradouro: data.logradouro,
+            regiao: data.regiao,
+            siafi: data.siafi,
+            uf: data.uf,
+            unidade: data.unidade
+        }
+        console.log(retorno);
+        return retorno;
+    } catch (erro) {
+        console.error("Nao foi possivel fazer contato com a API: ", erro);
+        return null;
+    }
+    
+}
