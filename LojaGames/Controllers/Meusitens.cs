e95cd1e92cb7c758f05e5024e9dccbdfd81de5c1
joaopauloo10101010 <joao_paulo_oandrade@hotmail.com>
2025-06-04 02:36:28 -0300

    adicionando nova view para os pedidos

---
+++ LojaGames/Controllers/Meusitens.cs
@@ -0,0 +1,30 @@
+﻿using LojaGames.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaGames.Controllers
+{
+    public class Meusitens : Controller
+    {
+        private readonly ProdutoRepositorio _produtoRepositorio;
+
+
+        public Meusitens(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Listamenu() 
+        {
+            _produtoRepositorio.listadeprodutoserdados.listadohistorico = _produtoRepositorio.getHistorico(HttpContext.Session.GetString("cpf"));
+            _produtoRepositorio.listadeprodutoserdados.listadeprodutos = _produtoRepositorio.ListaProdutos("Xbox");
+            _produtoRepositorio.listadeprodutoserdados.listacarrinho = _produtoRepositorio.listaCarrinho(HttpContext.Session.GetString("Pedido"));
+            return View(_produtoRepositorio.listadeprodutoserdados);
+        
+        }
+    }
+}
